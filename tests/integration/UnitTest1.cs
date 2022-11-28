e2bf0313d44a4bc380b203b5015a87eb05c9d3d3
Aganur Ahundov <aah@jooble.com>
2022-11-28 23:05:20 +0400

    Add integration test

---
+++ tests/integration/UnitTest1.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace integration;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        Console.WriteLine("Connecting to database...");
+    }
+}
