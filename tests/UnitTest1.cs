f0c72e5d53d5af9dc2028f0534395e04d28bca23
Aganur Ahundov <aah@jooble.com>
2022-11-28 22:58:49 +0400

    Add unit tests project

---
+++ tests/UnitTest1.cs
@@ -0,0 +1,20 @@
+using System;
+using Xunit;
+
+namespace Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        Console.WriteLine("Test1 processing...");
+    }
+
+
+    [Fact]
+    public void Test2()
+    {
+        Console.WriteLine("Test2 processing...");
+    }
+}
